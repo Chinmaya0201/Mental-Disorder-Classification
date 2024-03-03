9294137a9cac0301f752aa3529b77b99d6b4e349
Chinmaya <g.chinmaya32@gmail.com>
2024-03-03 13:44:15 +0530

    Create data_ingestion file

--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,7 @@ import os
 import logging
 from datetime import datetime 
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H')}.log" # -> name of the log file in the format which would be inside the log folder
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H')}.log" # -> name of the log file in the format which would be inside the logs folder
 logs_path = os.path.join(os.getcwd(),'LOGS', os.path.splitext(LOG_FILE)[0]) # Creating the complete path which includes current working directory+name of the folder "LOGS"+ name of the file we created above
 os.makedirs(logs_path, exist_ok= True) # Creating those directory
 
@@ -10,7 +10,8 @@ LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
     filename= LOG_FILE_PATH,
-    format= "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format="[%(asctime)s] file name = %(pathname)s in line number %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    # format to have time, filename, line number and error message  
     level= logging.INFO
 )
 
